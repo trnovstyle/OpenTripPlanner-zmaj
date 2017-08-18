9b7e594a7be171ea5971ec489915c3f5992705e7
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2017-08-18 14:39:58 +0200

    Fixed stop linking to only one edge of platform (#2472)

--- src/main/java/org/opentripplanner/graph_builder/module/StreetLinkerModule.java
+++ src/main/java/org/opentripplanner/graph_builder/module/StreetLinkerModule.java
@@ -33,6 +33,8 @@ public class StreetLinkerModule implements GraphBuilderModule {
 
     private static final Logger LOG = LoggerFactory.getLogger(StreetLinkerModule.class);
 
+    public Boolean skipVisibility = false;
+
     public List<String> provides() {
         return Arrays.asList("street to transit", "linking");
     }
@@ -48,6 +50,7 @@ public class StreetLinkerModule implements GraphBuilderModule {
         if(graph.hasStreets) {
             LOG.info("Linking transit stops, bike rental stations, bike parking areas, and park-and-rides to graph . . .");
             SimpleStreetSplitter linker = new SimpleStreetSplitter(graph);
+            linker.skipVisibilty = this.skipVisibility;
             linker.link();
         }
         //Calculates convex hull of a graph which is shown in routerInfo API point
