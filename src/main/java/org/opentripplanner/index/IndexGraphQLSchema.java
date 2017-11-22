1092b199cc8c35b8512a68e7345345035cf5f03f
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2017-11-22 14:32:19 +0100

    Notices fix in NeTEx schema
    (cherry picked from commit b50632a)

--- src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
+++ src/main/java/org/opentripplanner/index/IndexGraphQLSchema.java
@@ -68,13 +68,9 @@ public class IndexGraphQLSchema {
     public static GraphQLEnumType realtimeStateEnum = GraphQLEnumType.newEnum()
         .name("RealtimeState")
         .value("SCHEDULED", RealTimeState.SCHEDULED, "The trip information comes from the GTFS feed, i.e. no real-time update has been applied.")
-
         .value("UPDATED", RealTimeState.UPDATED, "The trip information has been updated, but the trip pattern stayed the same as the trip pattern of the scheduled trip.")
-
         .value("CANCELED", RealTimeState.CANCELED, "The trip has been canceled by a real-time update.")
-
         .value("ADDED", RealTimeState.ADDED, "The trip has been added using a real-time update, i.e. the trip was not present in the GTFS feed.")
-
         .value("MODIFIED", RealTimeState.MODIFIED, "The trip information has been updated and resulted in a different trip pattern compared to the trip pattern of the scheduled trip.")
         .build();
 
@@ -143,19 +139,19 @@ public class IndexGraphQLSchema {
         noticeType = GraphQLObjectType.newObject()
                 .name("Notice")
                 .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("Id")
+                        .name("id")
                         .type(Scalars.GraphQLString)
                         .dataFetcher(
                                 environment -> ((Notice) environment.getSource()).getId())
                         .build())
                 .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("Text")
+                        .name("text")
                         .type(Scalars.GraphQLString)
                         .dataFetcher(
                                 environment -> ((Notice) environment.getSource()).getText())
                         .build())
                 .field(GraphQLFieldDefinition.newFieldDefinition()
-                        .name("PublicCode")
+                        .name("publicCode")
                         .type(Scalars.GraphQLString)
                         .dataFetcher(
                                 environment -> ((Notice) environment.getSource()).getPublicCode())
