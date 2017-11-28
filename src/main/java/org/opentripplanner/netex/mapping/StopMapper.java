choesang/OpenTripPlanner

Fallback to earlier version of stop if quay not found

src/main/java/org/opentripplanner/netex/mapping/StopMapper.java
@@ -1,6 +1,8 @@
 package org.opentripplanner.netex.mapping;
 
+import com.google.common.collect.Iterables;
 import org.opentripplanner.model.Stop;
+import org.opentripplanner.model.impl.OtpTransitDaoBuilder;
 import org.rutebanken.netex.model.Quay;
 import org.rutebanken.netex.model.StopPlace;
 import org.slf4j.Logger;
@@ -8,56 +10,85 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StopMapper {
     private static final Logger LOG = LoggerFactory.getLogger(StopMapper.class);
 
-    public Collection<Stop> mapParentAndChildStops(StopPlace stopPlace, Map<String, StopPlace> parentStopPlacesById){
+    public Collection<Stop> mapParentAndChildStops(Collection<StopPlace> stopPlaceAllVersions, Map<String, StopPlace> parentStopPlacesById){
         ArrayList<Stop> stops = new ArrayList<>();
 
         Stop stop = new Stop();
         stop.setLocationType(1);
-        if (stopPlace.getName() != null) {
-            stop.setName(stopPlace.getName().getValue());
-        } else if (stopPlace.getParentSiteRef() != null && parentStopPlacesById.containsKey(stopPlace.getParentSiteRef().getRef())) {
-            String parentName = parentStopPlacesById.get(stopPlace.getParentSiteRef().getRef()).getName().getValue();
+
+        // Sort by versions, latest first
+        stopPlaceAllVersions = stopPlaceAllVersions.stream()
+                .sorted((o1, o2) -> Integer.compare(Integer.parseInt(o2.getVersion()), Integer.parseInt(o1.getVersion())))
+                .collect(Collectors.toList());
+
+        StopPlace stopPlaceLatest = Iterables.getLast(stopPlaceAllVersions);
+
+        if (stopPlaceLatest.getName() != null) {
+            stop.setName(stopPlaceLatest.getName().getValue());
+        } else if (stopPlaceLatest.getParentSiteRef() != null && parentStopPlacesById.containsKey(stopPlaceLatest.getParentSiteRef().getRef())) {
+            String parentName = parentStopPlacesById.get(stopPlaceLatest.getParentSiteRef().getRef()).getName().getValue();
             if (parentName != null) {
                 stop.setName(parentName);
             } else {
-                LOG.warn("No name found for stop " + stopPlace.getId() + " or in parent stop");
+                LOG.warn("No name found for stop " + stopPlaceLatest.getId() + " or in parent stop");
                 stop.setName("Not found");
             }
         } else {
-
-            LOG.warn("No name found for stop " + stopPlace.getId());
+            LOG.warn("No name found for stop " + stopPlaceLatest.getId() + " or in parent stop");
             stop.setName("Not found");
         }
-        if(stopPlace.getCentroid() != null){
-            stop.setLat(stopPlace.getCentroid().getLocation().getLatitude().doubleValue());
-            stop.setLon(stopPlace.getCentroid().getLocation().getLongitude().doubleValue());
+
+        if(stopPlaceLatest.getCentroid() != null){
+            stop.setLat(stopPlaceLatest.getCentroid().getLocation().getLatitude().doubleValue());
+            stop.setLon(stopPlaceLatest.getCentroid().getLocation().getLongitude().doubleValue());
         }else{
-            LOG.warn(stopPlace.getId() + " does not contain any coordinates.");
+            LOG.warn(stopPlaceLatest.getId() + " does not contain any coordinates.");
         }
 
-        stop.setId(AgencyAndIdFactory.getAgencyAndId(stopPlace.getId()));
+        stop.setId(AgencyAndIdFactory.getAgencyAndId(stopPlaceLatest.getId()));
         stops.add(stop);
-        List<Object> quayRefOrQuay = stopPlace.getQuays().getQuayRefOrQuay();
-        for(Object quayObject : quayRefOrQuay){
-            if(quayObject instanceof Quay){
-                Quay quay = (Quay) quayObject;
-                Stop stopQuay = new Stop();
-                stopQuay.setLocationType(0);
-                stopQuay.setName(stop.getName());
-                stopQuay.setLat(quay.getCentroid().getLocation().getLatitude().doubleValue());
-                stopQuay.setLon(quay.getCentroid().getLocation().getLongitude().doubleValue());
-                stopQuay.setId(AgencyAndIdFactory.getAgencyAndId(quay.getId()));
-                stopQuay.setParentStation(stop.getId().getId());
-                stops.add(stopQuay);
+
+        // Get quays from all versions of stop place
+        Set<String> quaysSeen = new HashSet<>();
+
+        for (StopPlace stopPlace : stopPlaceAllVersions) {
+            if (stopPlace.getQuays() != null) {
+                List<Object> quayRefOrQuay = stopPlace.getQuays().getQuayRefOrQuay();
+                for (Object quayObject : quayRefOrQuay) {
+                    if (quayObject instanceof Quay) {
+                        Quay quay = (Quay) quayObject;
+                        Stop stopQuay = new Stop();
+                        stopQuay.setLocationType(0);
+                        if (quay.getCentroid() == null || quay.getCentroid().getLocation() == null
+                                || quay.getCentroid().getLocation().getLatitude() == null
+                                || quay.getCentroid().getLocation().getLatitude() == null) {
+                            LOG.warn("Quay " + quay.getId() + " does not contain any coordinates.");
+                            continue;
+                        }
+                        stopQuay.setName(stop.getName());
+                        stopQuay.setLat(quay.getCentroid().getLocation().getLatitude().doubleValue());
+                        stopQuay.setLon(quay.getCentroid().getLocation().getLongitude().doubleValue());
+                        stopQuay.setId(AgencyAndIdFactory.getAgencyAndId(quay.getId()));
+                        stopQuay.setParentStation(stop.getId().getId());
+
+                        if (!quaysSeen.contains(quay.getId())) {
+                            stops.add(stopQuay);
+                            quaysSeen.add(quay.getId());
+                        }
+                    }
+                }
             }
         }
-
         return stops;
     }
 }
+

trnovstyle/OpenTripPlanner-zmaj

src/main/java/org/opentripplanner/netex/mapping/StopMapper.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.netex.mapping;
 
+import com.google.common.collect.Iterables;
 import org.opentripplanner.model.Stop;
 import org.rutebanken.netex.model.Quay;
 import org.rutebanken.netex.model.StopPlace;
@@ -8,56 +9,85 @@ import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class StopMapper {
     private static final Logger LOG = LoggerFactory.getLogger(StopMapper.class);
 
-    public Collection<Stop> mapParentAndChildStops(StopPlace stopPlace, Map<String, StopPlace> parentStopPlacesById){
+    public Collection<Stop> mapParentAndChildStops(Collection<StopPlace> stopPlaceAllVersions, Map<String, StopPlace> parentStopPlacesById){
         ArrayList<Stop> stops = new ArrayList<>();
 
         Stop stop = new Stop();
         stop.setLocationType(1);
-        if (stopPlace.getName() != null) {
-            stop.setName(stopPlace.getName().getValue());
-        } else if (stopPlace.getParentSiteRef() != null && parentStopPlacesById.containsKey(stopPlace.getParentSiteRef().getRef())) {
-            String parentName = parentStopPlacesById.get(stopPlace.getParentSiteRef().getRef()).getName().getValue();
+
+        // Sort by versions, latest first
+        stopPlaceAllVersions = stopPlaceAllVersions.stream()
+                .sorted((o1, o2) -> Integer.compare(Integer.parseInt(o2.getVersion()), Integer.parseInt(o1.getVersion())))
+                .collect(Collectors.toList());
+
+        StopPlace stopPlaceLatest = Iterables.getLast(stopPlaceAllVersions);
+
+        if (stopPlaceLatest.getName() != null) {
+            stop.setName(stopPlaceLatest.getName().getValue());
+        } else if (stopPlaceLatest.getParentSiteRef() != null && parentStopPlacesById.containsKey(stopPlaceLatest.getParentSiteRef().getRef())) {
+            String parentName = parentStopPlacesById.get(stopPlaceLatest.getParentSiteRef().getRef()).getName().getValue();
             if (parentName != null) {
                 stop.setName(parentName);
             } else {
-                LOG.warn("No name found for stop " + stopPlace.getId() + " or in parent stop");
+                LOG.warn("No name found for stop " + stopPlaceLatest.getId() + " or in parent stop");
                 stop.setName("Not found");
             }
         } else {
-
-            LOG.warn("No name found for stop " + stopPlace.getId());
+            LOG.warn("No name found for stop " + stopPlaceLatest.getId() + " or in parent stop");
             stop.setName("Not found");
         }
-        if(stopPlace.getCentroid() != null){
-            stop.setLat(stopPlace.getCentroid().getLocation().getLatitude().doubleValue());
-            stop.setLon(stopPlace.getCentroid().getLocation().getLongitude().doubleValue());
+
+        if(stopPlaceLatest.getCentroid() != null){
+            stop.setLat(stopPlaceLatest.getCentroid().getLocation().getLatitude().doubleValue());
+            stop.setLon(stopPlaceLatest.getCentroid().getLocation().getLongitude().doubleValue());
         }else{
-            LOG.warn(stopPlace.getId() + " does not contain any coordinates.");
+            LOG.warn(stopPlaceLatest.getId() + " does not contain any coordinates.");
         }
 
-        stop.setId(AgencyAndIdFactory.createAgencyAndId(stopPlace.getId()));
+        stop.setId(AgencyAndIdFactory.createAgencyAndId(stopPlaceLatest.getId()));
         stops.add(stop);
-        List<Object> quayRefOrQuay = stopPlace.getQuays().getQuayRefOrQuay();
-        for(Object quayObject : quayRefOrQuay){
-            if(quayObject instanceof Quay){
-                Quay quay = (Quay) quayObject;
-                Stop stopQuay = new Stop();
-                stopQuay.setLocationType(0);
-                stopQuay.setName(stop.getName());
-                stopQuay.setLat(quay.getCentroid().getLocation().getLatitude().doubleValue());
-                stopQuay.setLon(quay.getCentroid().getLocation().getLongitude().doubleValue());
-                stopQuay.setId(AgencyAndIdFactory.createAgencyAndId(quay.getId()));
-                stopQuay.setParentStation(stop.getId().getId());
-                stops.add(stopQuay);
+
+        // Get quays from all versions of stop place
+        Set<String> quaysSeen = new HashSet<>();
+
+        for (StopPlace stopPlace : stopPlaceAllVersions) {
+            if (stopPlace.getQuays() != null) {
+                List<Object> quayRefOrQuay = stopPlace.getQuays().getQuayRefOrQuay();
+                for (Object quayObject : quayRefOrQuay) {
+                    if (quayObject instanceof Quay) {
+                        Quay quay = (Quay) quayObject;
+                        Stop stopQuay = new Stop();
+                        stopQuay.setLocationType(0);
+                        if (quay.getCentroid() == null || quay.getCentroid().getLocation() == null
+                                || quay.getCentroid().getLocation().getLatitude() == null
+                                || quay.getCentroid().getLocation().getLatitude() == null) {
+                            LOG.warn("Quay " + quay.getId() + " does not contain any coordinates.");
+                            continue;
+                        }
+                        stopQuay.setName(stop.getName());
+                        stopQuay.setLat(quay.getCentroid().getLocation().getLatitude().doubleValue());
+                        stopQuay.setLon(quay.getCentroid().getLocation().getLongitude().doubleValue());
+                        stopQuay.setId(AgencyAndIdFactory.createAgencyAndId(quay.getId()));
+                        stopQuay.setParentStation(stop.getId().getId());
+
+                        if (!quaysSeen.contains(quay.getId())) {
+                            stops.add(stopQuay);
+                            quaysSeen.add(quay.getId());
+                        }
+                    }
+                }
             }
         }
-
         return stops;
     }
 }
+