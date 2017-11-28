choesang/OpenTripPlanner

Import Netex data into OtpTransModel and build graph with PatternHopFactory

src/main/java/org/opentripplanner/netex/mapping/RouteMapper.java
@@ -1,0 +1,33 @@
+package org.opentripplanner.netex.mapping;
+
+import org.opentripplanner.model.Agency;
+import org.opentripplanner.model.impl.OtpTransitDaoBuilder;
+import org.rutebanken.netex.model.Line;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RouteMapper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RouteMapper.class);
+
+    TransportTypeMapper transportTypeMapper = new TransportTypeMapper();
+
+    public org.opentripplanner.model.Route mapRoute(Line line, OtpTransitDaoBuilder transitBuilder){
+
+        org.opentripplanner.model.Route otpRoute = new org.opentripplanner.model.Route();
+
+        if (line.getOperatorRef() == null) {
+            LOG.warn("Line " + line.getId() + " does not have an operator.");
+        } else {
+            String agencyId = line.getOperatorRef().getRef();
+            Agency agency = transitBuilder.getAgencies().stream().filter(a -> a.getId().equals(agencyId)).findFirst().get();
+            otpRoute.setAgency(agency);
+        }
+        otpRoute.setId(AgencyAndIdFactory.getAgencyAndId(line.getId()));
+        otpRoute.setLongName(line.getName().getValue());
+        otpRoute.setShortName(line.getPublicCode());
+        otpRoute.setType(transportTypeMapper.mapTransportType(line.getTransportMode().value()));
+
+        return otpRoute;
+    }
+}

src/main/java/org/opentripplanner/netex/mapping/StopMapper.java
@@ -1,0 +1,63 @@
+package org.opentripplanner.netex.mapping;
+
+import org.opentripplanner.model.Stop;
+import org.rutebanken.netex.model.Quay;
+import org.rutebanken.netex.model.StopPlace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class StopMapper {
+    private static final Logger LOG = LoggerFactory.getLogger(StopMapper.class);
+
+    public Collection<Stop> mapParentAndChildStops(StopPlace stopPlace, Map<String, StopPlace> parentStopPlacesById){
+        ArrayList<Stop> stops = new ArrayList<>();
+
+        Stop stop = new Stop();
+        stop.setLocationType(1);
+        if (stopPlace.getName() != null) {
+            stop.setName(stopPlace.getName().getValue());
+        } else if (stopPlace.getParentSiteRef() != null && parentStopPlacesById.containsKey(stopPlace.getParentSiteRef().getRef())) {
+            String parentName = parentStopPlacesById.get(stopPlace.getParentSiteRef().getRef()).getName().getValue();
+            if (parentName != null) {
+                stop.setName(parentName);
+            } else {
+                LOG.warn("No name found for stop " + stopPlace.getId() + " or in parent stop");
+                stop.setName("Not found");
+            }
+        } else {
+
+            LOG.warn("No name found for stop " + stopPlace.getId());
+            stop.setName("Not found");
+        }
+        if(stopPlace.getCentroid() != null){
+            stop.setLat(stopPlace.getCentroid().getLocation().getLatitude().doubleValue());
+            stop.setLon(stopPlace.getCentroid().getLocation().getLongitude().doubleValue());
+        }else{
+            LOG.warn(stopPlace.getId() + " does not contain any coordinates.");
+        }
+
+        stop.setId(AgencyAndIdFactory.getAgencyAndId(stopPlace.getId()));
+        stops.add(stop);
+        List<Object> quayRefOrQuay = stopPlace.getQuays().getQuayRefOrQuay();
+        for(Object quayObject : quayRefOrQuay){
+            if(quayObject instanceof Quay){
+                Quay quay = (Quay) quayObject;
+                Stop stopQuay = new Stop();
+                stopQuay.setLocationType(0);
+                stopQuay.setName(stop.getName());
+                stopQuay.setLat(quay.getCentroid().getLocation().getLatitude().doubleValue());
+                stopQuay.setLon(quay.getCentroid().getLocation().getLongitude().doubleValue());
+                stopQuay.setId(AgencyAndIdFactory.getAgencyAndId(quay.getId()));
+                stopQuay.setParentStation(stop.getId().getId());
+                stops.add(stopQuay);
+            }
+        }
+
+        return stops;
+    }
+}

src/main/java/org/opentripplanner/netex/mapping/TripMapper.java
@@ -1,0 +1,61 @@
+package org.opentripplanner.netex.mapping;
+
+import org.opentripplanner.model.impl.OtpTransitDaoBuilder;
+import org.opentripplanner.model.Trip;
+import org.opentripplanner.graph_builder.model.NetexDao;
+import org.rutebanken.netex.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.xml.bind.JAXBElement;
+
+/**
+ * Agency id must be added when the stop is related to a line
+ */
+
+public class TripMapper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(TripMapper.class);
+
+    public Trip mapServiceJourney(ServiceJourney serviceJourney, OtpTransitDaoBuilder gtfsDao, NetexDao netexDao){
+
+        JAXBElement<? extends LineRefStructure> lineRefStruct = serviceJourney.getLineRef();
+
+        String lineRef = null;
+        if(lineRefStruct != null){
+            lineRef = lineRefStruct.getValue().getRef();
+        }else if(serviceJourney.getJourneyPatternRef() != null){
+            JourneyPattern journeyPattern = netexDao.getJourneyPatternsById().get(serviceJourney.getJourneyPatternRef().getValue().getRef());
+            String routeRef = journeyPattern.getRouteRef().getRef();
+            lineRef = netexDao.getRouteById().get(routeRef).getLineRef().getValue().getRef();
+        }
+
+        Trip trip = new Trip();
+        trip.setId(AgencyAndIdFactory.getAgencyAndId(serviceJourney.getId()));
+
+        trip.setRoute(gtfsDao.getRoutes().get(AgencyAndIdFactory.getAgencyAndId(lineRef)));
+        DayTypeRefs_RelStructure dayTypes = serviceJourney.getDayTypes();
+
+        StringBuilder serviceId = new StringBuilder();
+        boolean first = true;
+        for(JAXBElement dt : dayTypes.getDayTypeRef()){
+            if(!first){
+                serviceId.append("+");
+            }
+            first = false;
+            if(dt.getValue() instanceof DayTypeRefStructure){
+                DayTypeRefStructure dayType = (DayTypeRefStructure) dt.getValue();
+                serviceId.append(dayType.getRef());
+            }
+        }
+
+        // Add all unique service ids to map. Used when mapping calendars later.
+        if (!netexDao.getServiceIds().containsKey(serviceId.toString())) {
+            netexDao.getServiceIds().put(serviceId.toString(), serviceId.toString());
+        }
+
+        trip.setServiceId(AgencyAndIdFactory.getAgencyAndId(serviceId.toString()));
+
+        return trip;
+    }
+}

trnovstyle/OpenTripPlanner-zmaj

src/main/java/org/opentripplanner/netex/mapping/RouteMapper.java
@@ -1,0 +1,33 @@
+package org.opentripplanner.netex.mapping;
+
+import org.opentripplanner.model.Agency;
+import org.opentripplanner.model.impl.OtpTransitBuilder;
+import org.rutebanken.netex.model.Line;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class RouteMapper {
+
+    private static final Logger LOG = LoggerFactory.getLogger(RouteMapper.class);
+
+    TransportTypeMapper transportTypeMapper = new TransportTypeMapper();
+
+    public org.opentripplanner.model.Route mapRoute(Line line, OtpTransitBuilder transitBuilder){
+
+        org.opentripplanner.model.Route otpRoute = new org.opentripplanner.model.Route();
+
+        if (line.getOperatorRef() == null) {
+            LOG.warn("Line " + line.getId() + " does not have an operator.");
+        } else {
+            String agencyId = line.getOperatorRef().getRef();
+            Agency agency = transitBuilder.getAgencies().stream().filter(a -> a.getId().equals(agencyId)).findFirst().get();
+            otpRoute.setAgency(agency);
+        }
+        otpRoute.setId(AgencyAndIdFactory.createAgencyAndId(line.getId()));
+        otpRoute.setLongName(line.getName().getValue());
+        otpRoute.setShortName(line.getPublicCode());
+        otpRoute.setType(transportTypeMapper.mapTransportType(line.getTransportMode().value()));
+
+        return otpRoute;
+    }
+}

src/main/java/org/opentripplanner/netex/mapping/StopMapper.java
@@ -1,0 +1,63 @@
+package org.opentripplanner.netex.mapping;
+
+import org.opentripplanner.model.Stop;
+import org.rutebanken.netex.model.Quay;
+import org.rutebanken.netex.model.StopPlace;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+
+public class StopMapper {
+    private static final Logger LOG = LoggerFactory.getLogger(StopMapper.class);
+
+    public Collection<Stop> mapParentAndChildStops(StopPlace stopPlace, Map<String, StopPlace> parentStopPlacesById){
+        ArrayList<Stop> stops = new ArrayList<>();
+
+        Stop stop = new Stop();
+        stop.setLocationType(1);
+        if (stopPlace.getName() != null) {
+            stop.setName(stopPlace.getName().getValue());
+        } else if (stopPlace.getParentSiteRef() != null && parentStopPlacesById.containsKey(stopPlace.getParentSiteRef().getRef())) {
+            String parentName = parentStopPlacesById.get(stopPlace.getParentSiteRef().getRef()).getName().getValue();
+            if (parentName != null) {
+                stop.setName(parentName);
+            } else {
+                LOG.warn("No name found for stop " + stopPlace.getId() + " or in parent stop");
+                stop.setName("Not found");
+            }
+        } else {
+
+            LOG.warn("No name found for stop " + stopPlace.getId());
+            stop.setName("Not found");
+        }
+        if(stopPlace.getCentroid() != null){
+            stop.setLat(stopPlace.getCentroid().getLocation().getLatitude().doubleValue());
+            stop.setLon(stopPlace.getCentroid().getLocation().getLongitude().doubleValue());
+        }else{
+            LOG.warn(stopPlace.getId() + " does not contain any coordinates.");
+        }
+
+        stop.setId(AgencyAndIdFactory.createAgencyAndId(stopPlace.getId()));
+        stops.add(stop);
+        List<Object> quayRefOrQuay = stopPlace.getQuays().getQuayRefOrQuay();
+        for(Object quayObject : quayRefOrQuay){
+            if(quayObject instanceof Quay){
+                Quay quay = (Quay) quayObject;
+                Stop stopQuay = new Stop();
+                stopQuay.setLocationType(0);
+                stopQuay.setName(stop.getName());
+                stopQuay.setLat(quay.getCentroid().getLocation().getLatitude().doubleValue());
+                stopQuay.setLon(quay.getCentroid().getLocation().getLongitude().doubleValue());
+                stopQuay.setId(AgencyAndIdFactory.createAgencyAndId(quay.getId()));
+                stopQuay.setParentStation(stop.getId().getId());
+                stops.add(stopQuay);
+            }
+        }
+
+        return stops;
+    }
+}

src/main/java/org/opentripplanner/netex/mapping/TripMapper.java
@@ -1,0 +1,57 @@
+package org.opentripplanner.netex.mapping;
+
+import org.opentripplanner.model.impl.OtpTransitBuilder;
+import org.opentripplanner.model.Trip;
+import org.opentripplanner.graph_builder.model.NetexDao;
+import org.rutebanken.netex.model.*;
+
+import javax.xml.bind.JAXBElement;
+
+/**
+ * Agency id must be added when the stop is related to a line
+ */
+
+public class TripMapper {
+
+    public Trip mapServiceJourney(ServiceJourney serviceJourney, OtpTransitBuilder gtfsDao, NetexDao netexDao){
+
+        JAXBElement<? extends LineRefStructure> lineRefStruct = serviceJourney.getLineRef();
+
+        String lineRef = null;
+        if(lineRefStruct != null){
+            lineRef = lineRefStruct.getValue().getRef();
+        }else if(serviceJourney.getJourneyPatternRef() != null){
+            JourneyPattern journeyPattern = netexDao.getJourneyPatternsById().get(serviceJourney.getJourneyPatternRef().getValue().getRef());
+            String routeRef = journeyPattern.getRouteRef().getRef();
+            lineRef = netexDao.getRouteById().get(routeRef).getLineRef().getValue().getRef();
+        }
+
+        Trip trip = new Trip();
+        trip.setId(AgencyAndIdFactory.createAgencyAndId(serviceJourney.getId()));
+
+        trip.setRoute(gtfsDao.getRoutes().get(AgencyAndIdFactory.createAgencyAndId(lineRef)));
+        DayTypeRefs_RelStructure dayTypes = serviceJourney.getDayTypes();
+
+        StringBuilder serviceId = new StringBuilder();
+        boolean first = true;
+        for(JAXBElement dt : dayTypes.getDayTypeRef()){
+            if(!first){
+                serviceId.append("+");
+            }
+            first = false;
+            if(dt.getValue() instanceof DayTypeRefStructure){
+                DayTypeRefStructure dayType = (DayTypeRefStructure) dt.getValue();
+                serviceId.append(dayType.getRef());
+            }
+        }
+
+        // Add all unique service ids to map. Used when mapping calendars later.
+        if (!netexDao.getServiceIds().containsKey(serviceId.toString())) {
+            netexDao.getServiceIds().put(serviceId.toString(), serviceId.toString());
+        }
+
+        trip.setServiceId(AgencyAndIdFactory.createAgencyAndId(serviceId.toString()));
+
+        return trip;
+    }
+}