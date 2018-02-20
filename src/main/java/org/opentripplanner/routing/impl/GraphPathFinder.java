fa62abb8908662ad8d7044ceeed71448a6ecc6e2
Gard Mellemstrand <gard.mellemstrand@soprasteria.com>
2018-02-20 12:40:45 +0100

    Changed log message of CompactLegsByReverseSearch failed

--- src/main/java/org/opentripplanner/routing/impl/GraphPathFinder.java
+++ src/main/java/org/opentripplanner/routing/impl/GraphPathFinder.java
@@ -174,7 +174,7 @@ public class GraphPathFinder {
                 try {
                     newPaths = compactLegsByReversedSearch(aStar, originalReq, options, newPaths, timeout, reversedSearchHeuristic);
                 } catch (Exception e) {
-                    LOG.warn("CompactLegsByReversedSearch failed.", e, originalReq);
+                    LOG.warn("CompactLegsByReversedSearch failed on request: " + originalReq.toString());
                 }
             }
 
