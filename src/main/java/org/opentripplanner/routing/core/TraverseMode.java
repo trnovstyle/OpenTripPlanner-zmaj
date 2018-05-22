79f7e219b2f1ee184e21efcb1cd65dfa5cc8670e
erlendnils1 <erlendnils1@gmail.com>
2018-05-22 13:00:28 +0200

    Revert "Revert "Revert "Revert "Added support for COACH as traverse mode""""
    This reverts commit e584048017fac34da6d9681cc43504d7611bd4c1.

--- src/main/java/org/opentripplanner/routing/core/TraverseMode.java
+++ src/main/java/org/opentripplanner/routing/core/TraverseMode.java
@@ -26,7 +26,8 @@ public enum TraverseMode {
     TRAM, SUBWAY, RAIL, BUS, FERRY,
     CABLE_CAR, GONDOLA, FUNICULAR,
     TRANSIT, LEG_SWITCH,
-    AIRPLANE, CAR_PARK, CAR_PICKUP;
+    AIRPLANE, CAR_PARK, CAR_PICKUP,
+    COACH;
 
     private static HashMap <Set<TraverseMode>, Set<TraverseMode>> setMap = 
             new HashMap <Set<TraverseMode>, Set<TraverseMode>>();
@@ -48,7 +49,7 @@ public enum TraverseMode {
     public boolean isTransit() {
         return this == TRAM || this == SUBWAY || this == RAIL || this == BUS || this == FERRY
                 || this == CABLE_CAR || this == GONDOLA || this == FUNICULAR || this == TRANSIT
-                || this == AIRPLANE;
+                || this == AIRPLANE || this == COACH;
     }
 
     public boolean isOnStreetNonTransit() {
