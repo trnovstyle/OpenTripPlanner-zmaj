dbdf46954b79bfb116bb5aa278b540f56190e225
erlendnils1 <erlendnils1@gmail.com>
2018-08-02 14:49:39 +0200

    Map JsonParseExceptions to http 400

--- src/main/java/org/opentripplanner/api/common/OTPExceptionMapper.java
+++ src/main/java/org/opentripplanner/api/common/OTPExceptionMapper.java
@@ -1,5 +1,6 @@
 package org.opentripplanner.api.common;
 
+import com.fasterxml.jackson.core.JsonParseException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,23 +8,47 @@ import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.ext.ExceptionMapper;
 import javax.ws.rs.ext.Provider;
+import java.util.HashMap;
+import java.util.Map;
 
 @Provider
 public class OTPExceptionMapper implements ExceptionMapper<Exception> {
     private static final Logger LOG = LoggerFactory.getLogger(OTPExceptionMapper.class);
 
+
+    private Map<Class<? extends Exception>, Integer> responseStatusPerExceptionClass = new HashMap<>();
+
+    public OTPExceptionMapper() {
+
+        responseStatusPerExceptionClass.put(JsonParseException.class, Response.Status.BAD_REQUEST.getStatusCode());
+    }
+
     public Response toResponse(Exception ex) {
         // Show the exception in the server log
         LOG.error("Unhandled exception", ex);
 
-        int statusCode = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
-        if (ex instanceof WebApplicationException)
-            statusCode = ((WebApplicationException)ex).getResponse().getStatus();
+
+        int statusCode = getStatusForException(ex);
 
         // Return the short form message to the client
         return Response.status(statusCode)
-                .entity(ex.toString() + " " + ex.getMessage())
-                .type("text/plain").build();
+                       .entity(ex.toString() + " " + ex.getMessage())
+                       .type("text/plain").build();
+    }
+
+    private <E extends Exception> int getStatusForException(E ex) {
+        int statusCode = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode(); // Default value
+
+        if (ex != null) {
+            if (ex instanceof WebApplicationException) {
+                statusCode = ((WebApplicationException) ex).getResponse().getStatus();
+            } else {
+                statusCode = responseStatusPerExceptionClass.entrySet().stream()
+                                     .filter(exClass -> exClass.getKey().isAssignableFrom(ex.getClass())).map(Map.Entry::getValue).findFirst().orElse(statusCode);
+            }
+        }
+
+        return statusCode;
     }
 
 }
