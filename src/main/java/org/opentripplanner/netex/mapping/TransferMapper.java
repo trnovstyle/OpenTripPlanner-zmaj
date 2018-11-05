02350a74989e1dea82b5b7a0fb7c89d1bad6bae0
Gard Mellemstrand <gard.mellemstrand@entur.org>
2018-11-05 16:20:08 +0100

    Added more checks for transfer mapping

--- src/main/java/org/opentripplanner/netex/mapping/TransferMapper.java
+++ src/main/java/org/opentripplanner/netex/mapping/TransferMapper.java
@@ -1,6 +1,9 @@
 package org.opentripplanner.netex.mapping;
 
+import org.opentripplanner.model.AgencyAndId;
+import org.opentripplanner.model.Stop;
 import org.opentripplanner.model.Transfer;
+import org.opentripplanner.model.Trip;
 import org.opentripplanner.model.impl.OtpTransitBuilder;
 import org.opentripplanner.netex.loader.NetexDao;
 import org.rutebanken.netex.model.ServiceJourneyInterchange;
@@ -26,22 +29,45 @@ public class TransferMapper {
         String fromStopId = netexDao.quayIdByStopPointRef.lookup(interchange.getFromPointRef().getRef());
         String toStopId = netexDao.quayIdByStopPointRef.lookup(interchange.getToPointRef().getRef());
 
-        transfer.setFromStop(
-                transitBuilder.getStops().get(AgencyAndIdFactory.createAgencyAndId(fromStopId)));
-        transfer.setToStop(
-                transitBuilder.getStops().get(AgencyAndIdFactory.createAgencyAndId(toStopId)));
+        if (fromStopId == null || toStopId == null) {
+            LOG.warn("Stops not found for transfer " + interchange.getId());
+            return null;
+        }
 
-        transfer.setFromTrip(transitBuilder.getTrips().get(AgencyAndIdFactory
-                .createAgencyAndId(interchange.getFromJourneyRef().getRef())));
-        transfer.setToTrip(transitBuilder.getTrips()
-                .get(AgencyAndIdFactory.createAgencyAndId(interchange.getToJourneyRef().getRef())));
+        AgencyAndId fromStopAgencyAndId = AgencyAndIdFactory.createAgencyAndId(fromStopId);
+        AgencyAndId toStopAgencyAndId = AgencyAndIdFactory.createAgencyAndId(toStopId);
+
+        Stop fromStop = transitBuilder.getStops().containsKey(fromStopAgencyAndId) ?  transitBuilder.getStops().get(fromStopAgencyAndId) : null;
+        Stop toStop = transitBuilder.getStops().containsKey(toStopAgencyAndId) ?  transitBuilder.getStops().get(toStopAgencyAndId) : null;
+
+        if (fromStop != null) {
+            transfer.setFromStop(fromStop);
+        }
+        if (toStop != null) {
+            transfer.setToStop(toStop);
+        }
+
+        Trip fromTrip = transitBuilder.getTrips().get(AgencyAndIdFactory
+                .createAgencyAndId(interchange.getFromJourneyRef().getRef()));
+        Trip toTrip = transitBuilder.getTrips()
+                .get(AgencyAndIdFactory.createAgencyAndId(interchange.getToJourneyRef().getRef()));
+
+        if (fromTrip != null) {
+            transfer.setFromTrip(transitBuilder.getTrips().get(AgencyAndIdFactory
+                    .createAgencyAndId(interchange.getFromJourneyRef().getRef())));
+        }
+        if (toTrip != null) {
+            transfer.setToTrip(transitBuilder.getTrips()
+                    .get(AgencyAndIdFactory.createAgencyAndId(interchange.getToJourneyRef().getRef())));
+        }
 
         if (transfer.getFromTrip() == null || transfer.getToTrip() == null) {
             LOG.warn("Trips not found for transfer " + interchange.getId());
+            return null;
         }
 
-        if (transfer.getFromTrip() == null || transfer.getToTrip() == null
-                || transfer.getToStop() == null || transfer.getFromTrip() == null) {
+        if (transfer.getFromStop() == null || transfer.getToStop() == null) {
+            LOG.warn("Stops not found for transfer " + interchange.getId());
             return null;
         }
 
