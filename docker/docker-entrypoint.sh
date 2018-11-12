8ec630410e3d6a5fec840102b2fba481d142b831
erlendnils1 <erlendnils1@gmail.com>
2018-11-12 14:02:53 +0100

    Fetch current graph file name/path from gcs file instead of etcd/hubot

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -2,30 +2,15 @@
 
 : ${GRAPH_FILE_TARGET_PATH="/code/otpdata/norway/Graph.obj"}
 : ${FILE_TMP_PATH="/tmp/graph_obj_from_gcs"}
-: ${ETCD_GRAPH_URL_ENDPOINT="http://etcd-client:2379/v2/keys/prod/otp/marduk.file?quorum=false&recursive=false&sorted=false"}
 # Notice ending slash here, it is correct
 : ${MARDUK_GCP_BASE="gs://marduk/"}
 
 echo "GRAPH_FILE_TARGET_PATH: $GRAPH_FILE_TARGET_PATH"
 
-FROM_ETCD=$( curl --silent -X GET "$ETCD_GRAPH_URL_ENDPOINT" )
-echo "From etcd: $FROM_ETCD"
-if [[ "x" != "x$FROM_ETCD" ]] ;
-then
-  FILENAME=$( echo $FROM_ETCD | jq '.node.value' | tr -d '"')
-fi
-
-if [[ "x" == "x$FILENAME" ]] ;
-then
-  FILENAME=$(wget --quiet http://hubot/hubot/marduk/filename -O -)
-  echo "Reading value from hubot instead of etcd as etcd cannot be connected."
-  echo "As hubot is backed by etcd, this is likely to fail..."
-  echo "This would only work if the etcd value does not exist, but that etcd is up..."
-  echo "From hubot: $FILENAME"
-fi
-
 echo "Activating marduk blobstore service account"
 /code/google-cloud-sdk/bin/gcloud auth activate-service-account --key-file /etc/marduk/marduk-blobstore-credentials.json
+
+FILENAME=$(gsutil cat ${MARDUK_GCP_BASE}graphs/current)
 
 DOWNLOAD="${MARDUK_GCP_BASE}${FILENAME}"
 echo "Downloading $DOWNLOAD"
