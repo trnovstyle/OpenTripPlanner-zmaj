9b87dc54a2a2154844c43d01d6b3c539a4ebdb66
gmellemstrand <gard.mellemstrand@entur.org>
2019-03-08 10:55:57 +0100

    Add 10s sleep to docker_entrypoint

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -5,6 +5,9 @@
 # Notice ending slash here, it is correct
 : ${MARDUK_GCP_BASE="gs://marduk/"}
 
+echo "Sleeping for 10 seconds..."
+sleep 10s
+
 echo "GRAPH_FILE_TARGET_PATH: $GRAPH_FILE_TARGET_PATH"
 
 echo "Activating marduk blobstore service account"
