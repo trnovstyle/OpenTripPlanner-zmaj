51f6e9a2cb370ea06c7d4f4b56f0262497dd52e3
Gard Mellemstrand <gard.mellemstrand@entur.org>
2019-03-18 14:55:05 +0100

    ROR-603 - Also add bikeParkNodes to main node map

--- src/main/java/org/opentripplanner/graph_builder/module/osm/OSMDatabase.java
+++ src/main/java/org/opentripplanner/graph_builder/module/osm/OSMDatabase.java
@@ -184,15 +184,12 @@ public class OSMDatabase implements OpenStreetMapContentHandler {
     public void addNode(OSMNode node) {
         if (node.isBikeRental()) {
             bikeRentalNodes.put(node.getId(), node);
-            return;
         }
         if (node.isBikeParking()) {
             bikeParkingNodes.put(node.getId(), node);
-            return;
         }
         if (!(waysNodeIds.contains(node.getId()) || areaNodeIds.contains(node.getId()) || node
                 .isStop()))
-            return;
 
         if (nodesById.containsKey(node.getId()))
             return;
