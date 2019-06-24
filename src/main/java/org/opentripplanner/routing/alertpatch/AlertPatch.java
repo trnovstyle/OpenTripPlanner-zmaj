a636a451483b250ba25fa0ed25d67f13874c1f69
Lasse Tyrihjell <lassetyr@gmail.com>
2019-06-24 14:07:10 +0200

    Alerts added to trip/route AND stop should not be added to entire trip/route - plus support for alerts attached to parent-stop/station

--- src/main/java/org/opentripplanner/routing/alertpatch/AlertPatch.java
+++ src/main/java/org/opentripplanner/routing/alertpatch/AlertPatch.java
@@ -127,20 +127,32 @@ public class AlertPatch implements Serializable {
             agency = this.agency != null ? agencies.get(this.agency) : null;
         }
         Route route = this.route != null ? graph.index.routeForId.get(this.route) : null;
-        Stop stop = this.stop != null ? graph.index.stopForId.get(this.stop) : null;
         Trip trip = this.trip != null ? graph.index.tripForId.get(this.trip) : null;
+
+        Stop stop;
+        if (this.stop != null) {
+            if (graph.index.stopForId.get(this.stop) != null) {
+                stop = graph.index.stopForId.get(this.stop);
+            } else {
+                stop = graph.index.stationForId.get(this.stop);
+            }
+        } else {
+            stop = null;
+        }
 
         if (route != null || trip != null || agency != null) {
             Collection<TripPattern> tripPatterns = null;
 
-            if (trip != null) {
+            if (trip != null && stop == null) {
+                // Add alert to entire trip
                 tripPatterns = new LinkedList<>();
                 TripPattern tripPattern = graph.index.patternForTrip.get(trip);
                 if (tripPattern != null) {
                     tripPatterns.add(tripPattern);
                 }
-            } else if (route != null) {
-		tripPatterns = graph.index.patternsForRoute.get(route).stream()
+            } else if (route != null && stop == null) {
+                // Add alert to entire route
+		        tripPatterns = graph.index.patternsForRoute.get(route).stream()
                   .filter(tripPattern -> {
                    if (direction != null && !direction.equals(tripPattern.getDirection())) {
                        return false;
