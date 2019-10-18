4f0bdee141ee1050299378cca4984c68cf9d18bb
Lasse Tyrihjell <lassetyr@gmail.com>
2019-10-18 10:40:44 +0200

    Adding hostname as deprecated field for debugging differences in realtime across cluster

--- src/main/java/org/opentripplanner/index/transmodel/TransmodelIndexGraphQLSchema.java
+++ src/main/java/org/opentripplanner/index/transmodel/TransmodelIndexGraphQLSchema.java
@@ -1,27 +1,63 @@
 package org.opentripplanner.index.transmodel;
 
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.Envelope;
-import org.locationtech.jts.geom.LineString;
 import graphql.Scalars;
 import graphql.relay.DefaultConnection;
 import graphql.relay.DefaultPageInfo;
 import graphql.relay.Relay;
 import graphql.relay.SimpleListConnection;
-import graphql.schema.*;
+import graphql.schema.GraphQLArgument;
+import graphql.schema.GraphQLEnumType;
+import graphql.schema.GraphQLFieldDefinition;
+import graphql.schema.GraphQLInputObjectField;
+import graphql.schema.GraphQLInputObjectType;
+import graphql.schema.GraphQLInterfaceType;
+import graphql.schema.GraphQLList;
+import graphql.schema.GraphQLNonNull;
+import graphql.schema.GraphQLObjectType;
+import graphql.schema.GraphQLOutputType;
+import graphql.schema.GraphQLScalarType;
+import graphql.schema.GraphQLSchema;
+import graphql.schema.GraphQLType;
+import graphql.schema.GraphQLTypeReference;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.tuple.Pair;
+import org.locationtech.jts.geom.Coordinate;
+import org.locationtech.jts.geom.Envelope;
+import org.locationtech.jts.geom.LineString;
 import org.opentripplanner.api.common.Message;
-import org.opentripplanner.api.model.*;
+import org.opentripplanner.api.model.AbsoluteDirection;
+import org.opentripplanner.api.model.Itinerary;
+import org.opentripplanner.api.model.Leg;
+import org.opentripplanner.api.model.Place;
+import org.opentripplanner.api.model.RelativeDirection;
+import org.opentripplanner.api.model.TripPlan;
+import org.opentripplanner.api.model.VertexType;
+import org.opentripplanner.api.model.WalkStep;
 import org.opentripplanner.gtfs.GtfsLibrary;
 import org.opentripplanner.index.model.StopTimesInPattern;
 import org.opentripplanner.index.model.TripTimeShort;
 import org.opentripplanner.index.transmodel.mapping.TransmodelMappingUtil;
 import org.opentripplanner.index.transmodel.model.TransmodelPlaceType;
-import org.opentripplanner.index.transmodel.model.scalars.*;
+import org.opentripplanner.index.transmodel.model.scalars.DateScalarFactory;
+import org.opentripplanner.index.transmodel.model.scalars.DateTimeScalarFactory;
+import org.opentripplanner.index.transmodel.model.scalars.GeoJSONCoordinatesScalar;
+import org.opentripplanner.index.transmodel.model.scalars.LocalTimeScalarFactory;
+import org.opentripplanner.index.transmodel.model.scalars.TimeScalarFactory;
 import org.opentripplanner.index.util.TripTimeShortHelper;
-import org.opentripplanner.model.*;
+import org.opentripplanner.model.Agency;
+import org.opentripplanner.model.AgencyAndId;
+import org.opentripplanner.model.BookingArrangement;
+import org.opentripplanner.model.Branding;
+import org.opentripplanner.model.KeyValue;
+import org.opentripplanner.model.Notice;
+import org.opentripplanner.model.Operator;
+import org.opentripplanner.model.Route;
+import org.opentripplanner.model.Stop;
+import org.opentripplanner.model.TariffZone;
+import org.opentripplanner.model.Transfer;
+import org.opentripplanner.model.TransmodelTransportSubmode;
+import org.opentripplanner.model.Trip;
 import org.opentripplanner.model.calendar.ServiceDate;
 import org.opentripplanner.routing.alertpatch.Alert;
 import org.opentripplanner.routing.alertpatch.AlertPatch;
@@ -49,7 +85,20 @@ import org.opentripplanner.util.model.EncodedPolylineBean;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.BitSet;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.Set;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -2045,6 +2094,13 @@ public class TransmodelIndexGraphQLSchema {
                         .description("Whether this call is part of a flexible trip. This means that arrival or departure " +
                                 "times are not scheduled but estimated within specified operating hours.")
                         .dataFetcher(environment -> ((TripTimeShort) environment.getSource()).isFlexible())
+                        .build())
+                .field(GraphQLFieldDefinition.newFieldDefinition()
+                        .name("hostname")
+                        .type(Scalars.GraphQLString)
+                        .deprecate("For debugging only")
+                        .description("Server name - for debugging only!")
+                        .dataFetcher(environment -> System.getenv("HOSTNAME"))
                         .build())
                 .build();
 
