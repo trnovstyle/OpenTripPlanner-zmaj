c0b03416d2d71dbe20a286d3b03b8d9dd56a549c
Thomas Gran <t2gran@gmail.com>
2019-12-02 01:47:49 +0100

    Add support for nested elements in ReflectionLibrary.

--- src/main/java/org/opentripplanner/reflect/ReflectionLibrary.java
+++ src/main/java/org/opentripplanner/reflect/ReflectionLibrary.java
@@ -1,6 +1,7 @@
 package org.opentripplanner.reflect;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
 
 public class ReflectionLibrary {
 
@@ -9,21 +10,49 @@ public class ReflectionLibrary {
         StringBuilder sb = new StringBuilder();
         Class<?> clazz = object.getClass();
         sb.append("Summarizing ");
-        sb.append(clazz.getSimpleName());
+        sb.append(clazz.getSimpleName()).append(" {");
         sb.append('\n');
-        for (Field field : clazz.getFields()) {
-            sb.append(field.getName());
-            sb.append(" = ");
-            try {
-                Object fieldValue = field.get(object);
-                String value = fieldValue == null ? "null" : fieldValue.toString();
-                sb.append(value);
-            } catch (IllegalAccessException ex) {
-                sb.append("(non-public)");
-            }
-            sb.append('\n');
-        }
+        addFields("  ", clazz, object, sb);
+        sb.append("}");
         return sb.toString();
     }
 
+    private static void addFields(String indent, Class<?> clazz, Object object, StringBuilder sb) {
+        // Exit if the recursion is to deep; more than 8 levels down
+        if(indent.length() > 16) { return; }
+        // Add 2 spaces to the margin for each nested call to this method
+        indent = indent + "  ";
+
+        for (Field field : clazz.getFields()) {
+            int modifiers = field.getModifiers();
+            if(Modifier.isStatic(modifiers)) { continue; }
+
+            try {
+                Class<?> fieldClass = field.getType();
+                Object fieldValue = field.get(object);
+
+                sb.append(indent);
+                sb.append(field.getName());
+
+                if(fieldValue != null && isOtpClass(fieldClass)) {
+                    sb.append(" {\n");
+                    addFields(indent, fieldClass, fieldValue, sb);
+                    sb.append(indent).append("}");
+                }
+                else {
+                    String value = fieldValue == null ? "null" : fieldValue.toString();
+                    sb.append(" = ");
+                    sb.append(value);
+                }
+                sb.append('\n');
+            }
+            catch (IllegalAccessException ex) {
+                sb.append(" = (non-public)\n");
+            }
+        }
+    }
+
+    private static boolean isOtpClass(Class<?> fieldClass) {
+        return fieldClass.getName().startsWith("org.opentripplanner.");
+    }
 }
