d8f0c7c45affacf76ee73c7e95ea8359a287f5a8
Lasse Tyrihjell <lassetyr@gmail.com>
2020-05-28 10:13:45 +0200

    Added simple retry-mechanism to handle connection-issues during SIRI-ET data initialization

--- src/main/java/org/opentripplanner/updater/siri/SiriEstimatedTimetableGooglePubsubUpdater.java
+++ src/main/java/org/opentripplanner/updater/siri/SiriEstimatedTimetableGooglePubsubUpdater.java
@@ -232,7 +232,25 @@ public class SiriEstimatedTimetableGooglePubsubUpdater extends ReadinessBlocking
 
         final EstimatedTimetableMessageReceiver receiver = new EstimatedTimetableMessageReceiver();
 
-        initializeData(dataInitializationUrl, receiver);
+        int sleepPeriod = 1000;
+        int attemptCounter = 1;
+        while (!isInitialized) {
+            try {
+                initializeData(dataInitializationUrl, receiver);
+
+            } catch (IOException e) {
+
+                sleepPeriod = sleepPeriod * 2;
+
+                LOG.warn("Caught IOException while initializing data, will retry after {} ms - attempt number {}.", sleepPeriod, attemptCounter++);
+
+                try {
+                    Thread.sleep(sleepPeriod);
+                } catch (InterruptedException interruptedException) {
+                    //Ignore
+                }
+            }
+        }
 
         Subscriber subscriber = null;
         while (true) {
