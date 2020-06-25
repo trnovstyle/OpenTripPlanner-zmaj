70b1241708d2dd1d18af3ea01a23b263af5fd03f
Jonathan Brynhagen <jonathan.brynhagen@purplescout.se>
2020-06-25 15:53:43 +0200

    OTP-2105 Logging to ApplicationInsights
    	- Added ApplicationInsights.xml
    	- Added logback.xml
    	- Modified dockerfile to copy insight libraries into jar
    	- Copying ApplicationInsights.xml, AI_Agent.xml and logback.xml into jar
    	- Read ApplicationInsights key from environment variables set from Infra
    	- Changed referenced location to logging configs in Dockerfiles
    	- Changed Insight environment variable reference

--- docker-build/docker/services/entrypoint.sh
+++ docker-build/docker/services/entrypoint.sh
@@ -23,7 +23,17 @@ else
   exit 1
 fi
 
+cd /code
+wget https://github.com/microsoft/ApplicationInsights-Java/releases/download/2.6.1/applicationinsights-agent-2.6.1.jar
+wget https://repo1.maven.org/maven2/com/microsoft/azure/applicationinsights-web-auto/2.6.1/applicationinsights-web-auto-2.6.1.jar
+wget https://repo1.maven.org/maven2/com/microsoft/azure/applicationinsights-logging-logback/2.6.1/applicationinsights-logging-logback-2.6.1.jar
+
+jar -xf applicationinsights-web-auto-2.6.1.jar && jar -xf applicationinsights-logging-logback-2.6.1.jar
+jar -uf /code/otp-shaded.jar com/
+jar -uf /code/otp-shaded.jar ApplicationInsights.xml
+jar -uf /code/otp-shaded.jar logback.xml
+
 echo "Start java OTP jar" >> /var/log/otp.log
 echo "Start java OTP jar"
 
-exec "$@"
+exec java -javaagent:/code/applicationinsights-agent-2.6.1.jar -DAPPINSIGHTS_INSTRUMENTATIONKEY=$applicationInsightsKey -Xms256m -Xmx6144m -jar /code/otp-shaded.jar --server --graphs /code/otpdata --router malmo
