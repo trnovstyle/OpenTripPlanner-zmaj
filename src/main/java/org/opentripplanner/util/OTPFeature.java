47478ca3936010c8884e8635adf94060b937aa73
Jonathan Brynhagen <jonathan.brynhagen@purplescout.se>
2021-11-09 09:56:54 +0100

    11225: Enabled features FlexRouting & SandboxAPITransmodelApi

--- src/main/java/org/opentripplanner/util/OTPFeature.java
+++ src/main/java/org/opentripplanner/util/OTPFeature.java
@@ -28,13 +28,13 @@ public enum OTPFeature {
     FlexRouting(false),
     FloatingBike(false),
     GoogleCloudStorage(false),
-    ReportApi(false),
+    ReportApi(true),
     SandboxAPITransmodelApi(true),
     SandboxAPILegacyGraphQLApi(false),
     SandboxAPIMapboxVectorTilesApi(false),
     SandboxExampleAPIGraphStatistics(false),
     SandboxAPIParkAndRideApi(false),
-    TransferAnalyzer(false),
+    TransferAnalyzer(true),
     DataOverlay(false);
 
     private static final Logger LOG = LoggerFactory.getLogger(OTPFeature.class);
