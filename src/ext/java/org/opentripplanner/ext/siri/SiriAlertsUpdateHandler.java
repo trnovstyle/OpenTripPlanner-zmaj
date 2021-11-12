2135e1d2ed4f9fc0b7ef3924ee476cf4c30e6afb
bartosz <bartosz.kruba@purplescout.se>
2021-11-12 11:26:49 +0100

    Fix for null pointer
    - Added null check in SiriAlertsUpdateHandler

--- src/ext/java/org/opentripplanner/ext/siri/SiriAlertsUpdateHandler.java
+++ src/ext/java/org/opentripplanner/ext/siri/SiriAlertsUpdateHandler.java
@@ -367,9 +367,11 @@ public class SiriAlertsUpdateHandler {
 
                         if (tripId != null) {
                             ServiceDate serviceDate = null;
+
                             if (dataFrameRef != null && dataFrameRef.getValue() != null) {
-                                ZonedDateTime startOfService = DateMapper.asStartOfService(LocalDate.parse(
-                                    dataFrameRef.getValue()), graph.getTimeZone().toZoneId());
+                                ZonedDateTimestartOfService = DateMapper.asStartOfService(
+                                        LocalDate.parse(dataFrameRef.getValue()), graph.getTimeZone().toZoneId()
+                                );
 
                                 serviceDate = new ServiceDate(startOfService.getYear(),
                                     startOfService.getMonthValue(),
@@ -378,24 +380,25 @@ public class SiriAlertsUpdateHandler {
 
                             }
 
+
                             if (!affectedStops.isEmpty()) {
                                 for (AffectedStopPointStructure affectedStop : affectedStops) {
-                                    FeedScopedId stop = siriFuzzyTripMatcher.getStop(affectedStop
-                                        .getStopPointRef()
-                                        .getValue());
+                                    FeedScopedId stop = siriFuzzyTripMatcher.getStop(affectedStop.getStopPointRef().getValue());
                                     if (stop == null) {
-                                        stop = new FeedScopedId(feedId,
-                                            affectedStop.getStopPointRef().getValue()
-                                        );
+                                        stop = new FeedScopedId(feedId, affectedStop.getStopPointRef().getValue()
+                                    );
                                     }
 
                                     alert.addEntity(new EntitySelector.StopAndTrip(stop, tripId, serviceDate));
                                 }
-                            }
-                            else {
+                                }
+                             else {
                                 alert.addEntity(new EntitySelector.Trip(tripId, serviceDate));
                             }
 
+                        } else {
+                            LOG.warn("Did not found a match for DatedVehicleJourneyRef: {}", datedVehicleJourneyRef);
+                            }
                         } else {
                             LOG.warn("Did not found a match for DatedVehicleJourneyRef: {}", datedVehicleJourneyRef);
                         }
