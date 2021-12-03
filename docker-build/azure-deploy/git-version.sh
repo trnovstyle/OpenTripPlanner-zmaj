4f3c1ea1e3762da23fe970b9481f2454a604476b
Jonathan Brynhagen <jonathan.brynhagen@purplescout.se>
2021-12-03 08:42:18 +0100

    11956: Restructured deploy process to avoid locking pipeline on errors

--- docker-build/azure-deploy/git-version.sh
+++ docker-build/azure-deploy/git-version.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-versionTag="$OTP_VERSION.RELEASE"
+version=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout)
+versionTag="$version.RELEASE"
 echo "New version : $versionTag"
 
 gitUser=$(git config user.name)
@@ -10,6 +11,27 @@ gitEmail=$(git config user.email)
 
 # Get rid of all local tags
 git -c http.extraheader="AUTHORIZATION: bearer $DEVOPS_ACCESSTOKEN" fetch --prune  origin --tags -f
+
+nexusVersion=$(curl -u resesok:$MVN_PASSWORD -X GET "https://nexus-dev.skanetrafiken.se/service/rest/v1/search?repository=maven-releases&group=se.skanetrafiken&name=resesok-otp&version=$version" | python -c '
+import json,sys;
+obj=json.load(sys.stdin);
+if obj["items"]:
+  print obj["items"][0]["version"]
+')
+
+if [[ "$nexusVersion" != "$version" ]]
+then
+  echo "Version not found on nexus, setting SKIP_DEPLOY to false"
+  echo "##vso[task.setvariable variable=SKIP_DEPLOY]false"
+else
+  echo "Version was found on nexus, setting SKIP_DEPLOY flag true."
+  echo "##vso[task.setvariable variable=SKIP_DEPLOY]true"
+fi
+
+if [ $(git tag -l $versionTag) ]; then
+  echo "Tag already exists, skipping."
+  exit 0
+fi
 
 # tag the master branch
 echo "# Create local tag $versionTag"
