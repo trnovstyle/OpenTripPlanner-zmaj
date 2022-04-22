083974783f55e8a3e6f6d7a201304c8bf42bb402
Hannes Junnila <hannes.junnila@gmail.com>
2022-04-22 12:53:39 +0300

    hack: Add train option for Sørlandsbanen.

--- src/main/java/org/opentripplanner/transit/raptor/api/request/RaptorRequestBuilder.java
+++ src/main/java/org/opentripplanner/transit/raptor/api/request/RaptorRequestBuilder.java
@@ -30,6 +30,10 @@ public class RaptorRequestBuilder<T extends RaptorTripSchedule> {
   private final DebugRequestBuilder debug;
   private SearchDirection searchDirection;
   private RaptorSlackProvider slackProvider;
+
+  /** HACK SØRLANDSBANEN */
+  public double extraSearchCoachReluctance;
+
   // Algorithm
   private RaptorProfile profile;
 
@@ -51,6 +55,9 @@ public class RaptorRequestBuilder<T extends RaptorTripSchedule> {
 
     // Debug
     this.debug = new DebugRequestBuilder(defaults.debug());
+
+    // HACK SØRLANDSBANEN
+    this.extraSearchCoachReluctance = defaults.extraSearchCoachReluctance;
   }
 
   public SearchParamsBuilder<T> searchParams() {
